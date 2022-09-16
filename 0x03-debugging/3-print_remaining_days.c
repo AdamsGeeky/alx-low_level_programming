3c0333d19fc45897d0347d1a8413c4434327c9e2
AdamsGeeky <muhdadamu0008@gmail.com>
2022-09-16 15:17:06 +0100

    corection

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -22,14 +22,14 @@ void print_remaining_days(int month, int day, int year)
 	}	
 	else
 	{
-	if (month == 2 && day == 60)
-	{
-		printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+		if (month == 2 && day == 60)
+		{
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+		}
+		else
+		{
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 365 - day);
+		}
 	}
-	else
-	{
-		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 365 - day);
-	}
-	}
-	}
+}
