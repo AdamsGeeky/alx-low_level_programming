829990d46005e764a9b497563803d2f14d3d1dee
AdamsGeeky <muhdadamu0008@gmail.com>
2022-09-30 16:28:12 +0100

    header normal

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,12 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-/**
- * File: main.h
- * Auth: AdamsGeeky 
- * Desc: Header file contaning the prototypes of all functions
- */
-
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -27,5 +18,3 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
-#endif
